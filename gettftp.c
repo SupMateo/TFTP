ce7dc41693be7e5be55af7445c476acf877e05f9
SupMateo <mateo.suprano@ensea.fr>
2023-12-11 10:53:57 +0100

    Question 2

--- gettftp.c
+++ gettftp.c
@@ -2,14 +2,26 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #define FD_TERMINAL_OUT 1
 #define BUFSIZE 1024
 
 int main(int argc, char **argv)
 {
-	char server[BUFSIZE];
+	struct addrinfo hints, *result, *rp;
+
+	memset(&hints, 0, sizeof(struct addrinfo)); 
+	hints.ai_family = AF_INET;     
+    hints.ai_socktype = SOCK_STREAM;
+
+	char hostname[BUFSIZE];
 	char file[BUFSIZE];
+
+	int status;
+	char* port = "23";
 
 	if(argc != 3)
 	{
@@ -17,10 +29,19 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		strncpy(server, argv[1], BUFSIZE - 1);
-        server[BUFSIZE - 1] = '\0';
+			
+	strncpy(hostname, argv[1], BUFSIZE - 1);
+    hostname[BUFSIZE - 1] = '\0';
 
-        strncpy(file, argv[2], BUFSIZE - 1);
-        file[BUFSIZE - 1] = '\0';
+    strncpy(file, argv[2], BUFSIZE - 1);
+    file[BUFSIZE - 1] = '\0';
+
+	status = getaddrinfo(hostname, port, &hints, &result);
+		if (status != 0) {
+			fprintf(stderr, "ERROR : %s\n", gai_strerror(status));
+			exit(EXIT_FAILURE);
+		}
+
+	freeaddrinfo(result);
 	}
 }
--- puttftp.c
+++ puttftp.c
@@ -2,25 +2,49 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #define FD_TERMINAL_OUT 1
 #define BUFSIZE 1024
 
 int main(int argc, char **argv)
 {
-	char server[BUFSIZE];
-	char file[BUFSIZE];
+
+	struct addrinfo hints, *result, *rp;
 	
+	memset(&hints, 0, sizeof(struct addrinfo)); 
+	hints.ai_family = AF_INET;     
+    hints.ai_socktype = SOCK_STREAM;
+
+	char hostname[BUFSIZE];
+	char file[BUFSIZE];
+
+	int status;
+	char* port = "23";
+
+	int status;
+
 	if(argc != 3)
 	{
 		write(FD_TERMINAL_OUT,"Syntax : ./puttftp.c hostname file\n",strlen("Syntaxe : ./gettftp.c hostname file\n"));
 	}
 	else
 	{
-		strncpy(server, argv[1], BUFSIZE - 1);
-        server[BUFSIZE - 1] = '\0';
+			
+	strncpy(hostname, argv[1], BUFSIZE - 1);
+    hostname[BUFSIZE - 1] = '\0';
 
-        strncpy(file, argv[2], BUFSIZE - 1);
-        file[BUFSIZE - 1] = '\0';
+    strncpy(file, argv[2], BUFSIZE - 1);
+    file[BUFSIZE - 1] = '\0';
+
+	status = getaddrinfo(hostname, port, &hints, &result);
+		if (status != 0) {
+			fprintf(stderr, "ERROR : %s\n", gai_strerror(status));
+			exit(EXIT_FAILURE);
+		}
+
+	freeaddrinfo(result);
 	}
 }
